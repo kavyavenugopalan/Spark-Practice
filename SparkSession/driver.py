60da5316e29089afcd0af390003bba9341b7f570
kavyavenugopalan <38486668+kavyavenugopalan@users.noreply.github.com>
2021-07-01 18:17:40 +0530

    Update driver.py

--- SparkSession/driver.py
+++ SparkSession/driver.py
@@ -1,7 +1,17 @@
 from pyspark.sql import *
-
+  #EITHER
 if __name__=="__main__":
   spark=SparkSession.builder \
         .appName("Hello Spark") \
         .master("local[3]") \
         .getOrCreate()
+  
+  #OR
+  
+#if __name__ == "__main__":
+  #conf = SparkConf()
+  #conf.set("spark.app.name","Hello Spark")
+  #conf.set("spark.master","local[3]")
+  #spark = SparkSession.builder \
+          #.config(conf=conf) \
+          #.getOrCreate()
