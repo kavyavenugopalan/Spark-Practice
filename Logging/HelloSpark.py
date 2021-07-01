d69f627e684291f4dae68504de811da9261b1226
kavyavenugopalan <38486668+kavyavenugopalan@users.noreply.github.com>
2021-07-01 17:45:09 +0530

    Update HelloSpark.py

--- Logging/HelloSpark.py
+++ Logging/HelloSpark.py
@@ -4,9 +4,11 @@ from lib.logger import Log4J
 
 if __name__=="__main__":
   spark=SparkSession.builder \
-  .appName("Hello Spark") \
-  .master("local[3]") \
-  .getOrCreate()
+    .appName("Hello Spark") \
+    .master("local[3]") \
+    .getOrCreate()
 
   logger=Log4j(spark)
   logger.info("starting")
+  logger.info("stop")
+  spark.stop()
