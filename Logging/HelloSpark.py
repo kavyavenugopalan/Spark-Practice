0a2bed8d49bb4211a4abcd81e84d9e1b0fac59eb
kavyavenugopalan <38486668+kavyavenugopalan@users.noreply.github.com>
2021-07-01 15:07:28 +0530

    Add files via upload

---
+++ Logging/HelloSpark.py
@@ -0,0 +1,12 @@
+from pyspark.sql import *
+
+from lib.logger import Log4j
+
+if __name__=="__main__":
+  spark=SparkSession.builder \
+  .appName("Hello Spark") \
+  .master("local[3]") \
+  .getOrCreate()
+
+  logger=Log4j(spark)
+  logger.info("starting")
